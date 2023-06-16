999ea58b6f8b329ebaac9e064a47d1a7e4808056
Abraham Sylvester <mrabblessedslyst4christ099@gmail.com>
2023-06-16 13:20:15 +0300

    parameterization

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,8 +5,8 @@ let loginPage = new LoginPage()
 
 it('login test', () => {
     loginPage.navigate('https://trytestingthis.netlify.app/')
-    loginPage.enterUsername()
-    loginPage.enterPassword()
+    loginPage.enterUsername('test')
+    loginPage.enterPassword('test')
     loginPage.clickLoginBtn()
     
 })
